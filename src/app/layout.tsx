7618b6717e2fa69e3ec904cfe3da12669cf4acf8
Faisal Habib <L1S22BSSE0140@UCP.EDU.PK>
2025-06-01 16:33:02 +0500

    Fix: Update configuration for Netlify deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,11 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <div className="relative w-full flex items-center justify-center ">
+      <body className={`${inter.className} antialiased`}>
+        <div className="relative w-full flex items-center justify-center">
           <Navbar />
-          </div>
+        </div>
         {children}
       </body>
     </html>
